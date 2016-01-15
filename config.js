b40839cbed2f8e4bce3a98dbe4682f116054b322
Rory Powis <rory@hikkendry.com>
2016-01-15 21:12:53 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+/**
+ * React Static Boilerplate
+ * https://github.com/koistya/react-static-boilerplate
+ * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
+ */
+
+export default {
+  title: 'Pusscat LOL Generator',
+  description: 'A little site for lols by a bb for a bb',
+  googleAnalyticsId: 'UA-XXXXX-X',
+};
+
