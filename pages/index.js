092dcfd4dc03c714404a82e59c4d7fb0a03a476d
Rory Powis <rory@hikkendry.com>
2016-01-23 17:02:49 +0100

    Update endpoint to load json file

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,7 @@ export default class extends Component {
   constructor(props) {
     super(props);
 
-    this.cx = '014343924111201567214:mctjyj4egne';
-    this.apiKey = 'AIzaSyDp08xnKZo4xuPgajY4ZHPi--_hbmx1_Rk';
-    this.endpoint = 'https://www.googleapis.com/customsearch/v1';
+    this.endpoint = 'https://raw.githubusercontent.com/rpowis/pusscat.lol/master';
 
     this.state = {
       data: null
@@ -29,16 +27,13 @@ export default class extends Component {
   }
 
   componentDidMount() {
-    const api = this.endpoint + '?key=' + this.apiKey + '&cx=' + this.cx;
     let keyword = this.isBirthday() ? 'birthday' : 'happy';
-    let query = encodeURIComponent(keyword + ' cat'.replace(/ /g, '+'));
-    let url =  api + '&imgSize=large&q=' + query;
+    let url = this.endpoint + '/data/' + keyword + '.json';
 
     this.fetchData(url);
   }
 
   fetchData(url) {
-
     (async () => {
       try {
         var response = await fetch(url);
@@ -60,11 +55,12 @@ export default class extends Component {
       return <Spinner/>;
     }
 
-    let data = this.state.data;
-    let images = Object.keys(data.items).map(item => {
-      let i = data.items[item];
-      if (i.pagemap) {
-        return i.pagemap.cse_image[0].src;
+    let images = Object.keys(this.state.data).map(item => {
+      let field = this.state.data[item].rgl_link;
+      let regex = /imgurl=(\S+)&imgrefurl/;
+      let img = field.match(regex);
+      if (img) {
+        return img[1];
       }
     });
 
